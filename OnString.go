853a86f317da15b219e75bd64a939ba008f9ba27
yangkequn <yangkequn@gmail.com>
2022-04-21 21:39:00 +0800

    add:Merge

--- OnString.go
+++ OnString.go
@@ -26,25 +26,38 @@ func RemoveItemFromString(str *string, item string) bool {
 //concat two list  to one,using "," as separator
 //items in s2 should be skipped if they are in s1
 func NonRedundantMerge(list1 *string, list2 string, toFront bool) bool {
-	var modified bool = false
-	if len(*list1) == 0 {
-		modified = *list1 != list2
-		*list1 = list2
-		return modified
-	}
 	if len(list2) == 0 {
 		return false
 	}
+	if len(*list1) == 0 {
+		*list1 = list2 //list2 已经不为0
+		return true
+	}
 	items := strings.Split(list2, ",")
+	kept := "" //kept 应当保留原先的顺序
 	for _, item := range items {
 		if !strings.Contains(*list1, item) {
-			if toFront {
-				*list1 = item + "," + *list1
-			} else {
-				*list1 = *list1 + "," + item
-			}
-			modified = true
+			kept = kept + "," + item
 		}
 	}
-	return modified
+	if toFront && kept != "" {
+		*list1 = kept + "," + *list1
+	} else if kept != "" {
+		*list1 = *list1 + "," + kept
+	}
+	return len(kept) > 0
+}
+func Merge(list1 *string, list2 string, toFront bool) bool {
+	if len(list2) == 0 {
+		return false
+	}
+	if len(*list1) == 0 {
+		return false
+	}
+	if toFront {
+		*list1 = list2 + "," + *list1
+	} else {
+		*list1 = *list1 + "," + list2
+	}
+	return true
 }

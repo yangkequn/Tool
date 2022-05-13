37b626701a5c45e95a7fd3ce3523f292450e3a15
yangkequn <yangkequn@gmail.com>
2022-05-13 20:20:37 +0800

    add redis api

--- RedisAPI.go
+++ RedisAPI.go
@@ -3,11 +3,82 @@ package Tool
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"math/rand"
 	"time"
 
 	"github.com/go-redis/redis/v8"
 )
+
+func RedisServerStart(c context.Context, rds *redis.Client, serviceNameKey string, callback func(string) (channel string, content string)) (err error) {
+	const BATCH_SIZE = 100
+	var (
+		jobOfLastMinutes int = 0
+		// minutes_now is the minutes of current time
+		minutes_now int = int(time.Now().Unix() / 60)
+		ins         []string
+	)
+
+	for true {
+		pipe := rds.Pipeline()
+		//redis 中的下标前后可达，不像c++,第一个可以到达，最后一个不可到达
+		ins, err = pipe.LRange(c, serviceNameKey, 0, BATCH_SIZE-1).Result()
+		pipe.LTrim(c, serviceNameKey, BATCH_SIZE, -1)
+		if _, err = pipe.Exec(c); err != nil {
+			time.Sleep(time.Millisecond * 100)
+			continue
+		}
+		//calculate the job
+		for _, param := range ins {
+			channel, content := callback(param)
+			if channel == "" || content == "" {
+				continue
+			}
+			pipe.RPush(c, channel, content)
+			jobOfLastMinutes++
+		}
+		pipe.Exec(c)
+
+		//print Job infomation
+		if int(time.Now().Unix()/60) != minutes_now {
+			minutes_now = int(time.Now().Unix() / 60)
+			jobOfLastMinutes = 0
+			fmt.Println(time.Now().String(), " redis service ", serviceNameKey, " last minutes job:", jobOfLastMinutes)
+		}
+
+	}
+	return
+}
+func JsonToStruct(value *string, result interface{}) error {
+	if err := json.Unmarshal([]byte(*value), result); err != nil {
+		return err
+	}
+	return nil
+}
+
+//RedisCall: 1.use RPush to push data to redis. 2.use BLPop to pop data from selected channel
+//return: error
+func RedisDo(c context.Context, rds *redis.Client, CmdRedisKey string, ResultRedisKey string, param interface{}) (value string, err error) {
+	var (
+		b       []byte
+		results []string
+	)
+
+	if b, err = json.Marshal(param); err != nil {
+		return "", err
+	}
+	ppl := rds.Pipeline()
+	ppl.RPush(c, CmdRedisKey, b)
+	//长期不执行的任务，抛弃
+	ppl.Expire(c, CmdRedisKey, time.Second*60)
+	if _, err := ppl.Exec(c); err != nil {
+		return "", err
+	}
+	if results, err = rds.BLPop(c, time.Second*5, ResultRedisKey).Result(); err != nil {
+		return "", err
+	}
+	return results[0], nil
+}
 
 // RedisAPI 通过redis调用API的一般框架
 func RedisAPI(c context.Context, rds *redis.Client, PubChannel string, SubChannel string, param interface{}, result interface{}) error {

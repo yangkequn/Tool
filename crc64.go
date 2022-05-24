80327a7aae942fcb53d1d024529e3fa1a48a062c
yangkequn <yangkequn@gmail.com>
2022-05-24 09:46:54 +0800

    add random ID

--- crc64.go
+++ crc64.go
@@ -1,8 +1,16 @@
 package Tool
 
 import (
+	"math/rand"
+
 	"github.com/cespare/xxhash/v2"
 )
+
+//Random stringID ,base64 encoding
+func RandomBase64ID() string {
+	id := int64(rand.Uint64())
+	return Int64ToString(id)
+}
 
 //Hash to string
 func HashToString(s string) string {
